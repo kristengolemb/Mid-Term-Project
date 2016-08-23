10f64eed797a8726b2d059470821534f37239718
Sam <Sam.Ableson.Jr@gmail.com>
2016-08-23 09:05:10 -0400

    Added commented shell of the main

--- src/POSTerminal.java
+++ src/POSTerminal.java
@@ -2,7 +2,36 @@
 public class POSTerminal {
 
 	public static void main(String[] args) {
-		
+//		"Welcome message"
+//		present short list of options       1.) menu
+//											2.) see cart
+//											3.) checkout
+//
+//		1.) menu - list available items  1 - apples
+//										 2 - bananas
+//										 3 - chips
+////										 etc...
+//										 add '?' after a number to query the item's description
+//										 type '0' or "esc" to go up a level
+//							 
+//						 if user adds an item prompt them "quantity?" and add their quantity to the cart
+//						 
+//		 2.) see cart - list each item with quantity and line total
+//						then list subtotal
+//						then list tax
+//						then list final total
+//						
+//						type '0' or "esc" to go up a level
+//						
+//		3.)  checkout - 1.) cash  --  prompt tender amount -- display change
+//						2.) credit  -- prompt 16 digit number, exp date, and cvv  --  display card number XXXXXXX0000 on final receipt
+//						3.) check --  take check account number -- display account number xxxxxxxxx0000 on final receipt
+//
+//		4.)  final display receipt with total line items, tax, total, payment type
+//	
+//	"Prompt if they'd like to make another purchase"
+//	"Thank you message"
+		 
 		
 	}
 
